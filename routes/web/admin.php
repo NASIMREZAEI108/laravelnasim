210af669d47f19917122cf51e25bfff8f14b6449
NASIMREZAEI108 <99584168+NASIMREZAEI108@users.noreply.github.com>
2022-07-13 18:58:41 +0430

    Update admin.php

--- routes/web/admin.php
+++ routes/web/admin.php
@@ -1,15 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-
-
 
 Route::get('/' , function() {
     return view('admin.index');
-
-    Route::resource('users' , 'UserController');
 });
+    Route::resource('users' , 'UserController');
+
 
 
 
